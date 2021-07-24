fb74037fbf60dc869b8c9eec4bff5adcf4198b86
Dushyant <devops@bydushyant.com>
2021-07-24 17:43:04 +0530

    Add Componant styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
     return (
-        <div>
+        <div className="expense-item">
             <div>Date</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Title</h2>
-                <div>Amount</div>
+                <div className="expense-item__price">Amount</div>
             </div>
         </div>
     )
