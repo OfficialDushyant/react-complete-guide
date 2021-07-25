b9f01b6d78187a684ca46787458a658dbc287c35
Dushyant <devops@bydushyant.com>
2021-07-25 16:11:23 +0530

    Refactor code with map function

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,19 +2,26 @@ import './Expenses.css'
 import ExpenseItem from "./ExpenseItem"
 
 function Expenses(props) {
-    const expenses = props.expenses
+    const expenses = props.expenses;
     return (
         <div className="expenses">
-        <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
+            {
+                expenses.map((item) => { 
+                    return (
+                        <ExpenseItem
+                        title={item.title}
+                        amount={item.amount}
+                        date={item.date}
+                    /> 
+                    )
+                })
+            }
+        
+      {/* <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
@@ -24,7 +31,7 @@ function Expenses(props) {
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      ></ExpenseItem> */}
 
         </div>
     )
