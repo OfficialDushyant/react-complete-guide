0be492278b830c4a507d957215d7dba7d3ad37de
Dushyant <devops@bydushyant.com>
2021-07-31 23:24:30 +0530

    Use card component for appling stylings

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -6,9 +6,9 @@ function ExpenseDate (props){
     const year  = props.date.getFullYear() 
     return (
         <div className="expense-date">
-            <div className="expense-date__day">{day}</div>
-            <div className="expense-date__years">{year}</div>
             <div className="expense-date__month">{month}</div>
+            <div className="expense-date__years">{year}</div>
+            <div className="expense-date__day">{day}</div>
         </div>
     )
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,12 @@
-import './Expenses.css'
+import Card from './Card'
 import ExpenseItem from "./ExpenseItem"
+
+import './Expenses.css'
 
 function Expenses(props) {
     const expenses = props.expenses;
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {
                 expenses.map((item) => { 
                     return (
@@ -16,24 +18,7 @@ function Expenses(props) {
                     )
                 })
             }
-        
-      {/* <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem> */}
-
-        </div>
+        </Card>
     )
 }
 
