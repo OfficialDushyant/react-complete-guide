7dc1e83a7d12e72a7d3d1e14a94914b8b0c26c66
Dushyant <devops@bydushyant.com>
2021-08-01 23:34:39 +0530

    listning to user input

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses"
 const App = () => {
   const expenses = [
@@ -23,7 +24,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+        <NewExpense/>
         <Expenses expenses={expenses}/>
     </div>
   );
